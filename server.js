69b88d8c84739ec15d9ab8662c57b91e2212aee8
Glitch (hello-express) <none>
2020-04-12 19:58:10 +0000

    🌻🏢 Checkpoint ./server.js:3349592/2508 ./package.json:3349592/33

--- server.js
+++ server.js
@@ -9,6 +9,85 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//TODO require auth for db operations
+
+// setup a new database
+// persisted using async file storage
+// Security note: the database is saved to the file `db.json` on the local filesystem.
+// It's deliberately placed in the `.data` directory which doesn't get copied if someone remixes the project.
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('.data/db.json');
+var db = low(adapter);
+
+// default user list
+db.defaults({ users: [
+      {"firstName":"John", "lastName":"Hancock"},
+      {"firstName":"Liz",  "lastName":"Smith"},
+      {"firstName":"Ahmed","lastName":"Khan"}
+    ]
+  }).write();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/users", function (request, response) {
+  var dbUsers=[];
+  var users = db.get('users').value() // Find all users in the collection
+  users.forEach(function(user) {
+    dbUsers.push([user.firstName,user.lastName]); // adds their info to the dbUsers value
+  });
+  response.send(dbUsers); // sends dbUsers back to the page
+});
+
+// creates a new entry in the users collection with the submitted values
+app.post("/users", function (request, response) {
+  db.get('users')
+    .push({ firstName: request.query.fName, lastName: request.query.lName })
+    .write()
+  console.log("New user inserted in the database");
+  response.sendStatus(200);
+});
+
+// removes entries from users and populates it with default users
+app.get("/reset", function (request, response) {
+  // removes all entries from the collection
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
+  
+  // default users inserted in the database
+  var users= [
+      {"firstName":"John", "lastName":"Hancock"},
+      {"firstName":"Liz",  "lastName":"Smith"},
+      {"firstName":"Ahmed","lastName":"Khan"}
+  ];
+  
+  users.forEach(function(user){
+    db.get('users')
+      .push({ firstName: user.firstName, lastName: user.lastName })
+      .write()
+  });
+  console.log("Default users added");
+  response.redirect("/");
+});
+
+// removes all entries from the collection
+app.get("/clear", function (request, response) {
+  // removes all entries from the collection
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
+  response.redirect("/");
+});
+
 // our default array of dreams
 const dreams = [
   "Find and count some sheep",
