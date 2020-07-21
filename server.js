b010179da6ba67f02fb3e2e6d7869f238ffff95f
Glitch (postman-api-starter) <none>
2020-07-21 17:05:45 +0000

    ☄️❄️ Checkpoint ./server.js:3349592/1441

--- server.js
+++ server.js
@@ -152,19 +152,27 @@ app.get("/matches", (request, response) => {
     },
     tutorial: {
       title: "You sent a request to retrieve all matches for the team! 🎉",
-      intro: "The demo API we're using for this course is a for a fictional sports team. The API manages match, player, and team data.",
+      intro: "The demo API we're using for this course is a for a fictional sports team. The API manages match, player, and team data. "+
+        "The request you just sent uses a `GET` which is for retrieving data.",
       steps: [
         {
-          note: "Look at the request URL"
+          note: "Look at the request URL. It should look like this `{{training-api}}/matches`. The URL is partly made up from a variable, "+
+          " referenced using double curly braces around `training-api` - hover over it to see the value, it's part of the collection."
         },
         {
-          note: "The request you sent to `/matches` returned the following data.",
+          note: "Open the **Console** along the bottom of the Postman window to see the address the request sent to. You can click a request "+
+            "in the Console to see the full detail of what happened when it sent - this is helpful when you're troubleshooting. Close and "+
+            "open the Console area whenever you find it useful."
+        },
+        {
+          note: "The request you sent to `/matches` returned the following data. It's an array of the matches currently in the database, "+
+            "including a few data values for each match.",
           raw_data: {
             matches: matches
           }
         }
       ],
-      next: "Now do this...",
+      next: "This request retrieved all matches, but you can also filter the matches using parameters.",
       pic:
         "https://assets.postman.com/postman-docs/postman-app-overview-response.jpg",
       raw_data: {
